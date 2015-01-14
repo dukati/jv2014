e866242fabebf7e7335b02da115fcf17b0093805
javi <javi@git.es>
2015-01-14 10:26:24 +0100

    inicio punto 3

---
+++ jv2014/src/modelo/SesionUsuario.java
@@ -0,0 +1,11 @@
+package modelo;
+
+public class SesionUsuario {
+
+	// Atributos
+	public Usuario usr; //materializa la relación de composición del modelo UML
+	public String fecha; 
+	
+	
+	} // class
+
